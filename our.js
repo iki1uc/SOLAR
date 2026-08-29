b67abb899e85f6fb20e11200e436733740576e3b
iki1uc <entertrainer@web.de>
2026-08-29 19:43:33 +0200

    Create our.js

---
+++ our.js
@@ -0,0 +1,14 @@
+<id core="OUR" type="sign" mode="use">
+    <sign>3Ø9Ø81</sign>
+    <use>
+        <user>origin.tmp</user>
+        <hold>mind QI per()</hold>
+        <mark>origin.point</mark>
+        <exit>FREI</exit>   // NEU
+    </use>
+    <dim>
+        <axis>ALUF</axis>   // NEU
+        <root>OUR.base</root>
+        <state>OFFEN</state> // NEU
+    </dim>
+</id>
